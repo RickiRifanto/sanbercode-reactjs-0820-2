1ee6b63c159219608f98c0f4a11ff103c794fa9f
RickiRifanto <70141142+RickiRifanto@users.noreply.github.com>
2020-09-04 22:02:19 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Tugas9 from './Tugas-9/tugas9.js'
+import DaftarBuah from './Tugas-10/Tugas10.js'
+
+function App() {
+  return (
+    <div>
+      <Tugas9 />
+      <DaftarBuah/>
+    </div>
+  );
+}
+
+export default App;
